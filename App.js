d9b5375137dc2f5bd386f95575e8b18fa20e801e
tenko <tenko@macbook-air-anna.home>
2022-09-14 00:07:25 +0200

    Uploading Navigation Tab Bar Changes Before Package Reset

--- App.js
+++ App.js
@@ -3,13 +3,11 @@ import Home from './screens/Home';
 import AboutUs from './screens/AboutUs';
 import Memes from './screens/Memes';
 import SignOut from './screens/SignOut';
-import BlurView from 'expo-blur';
-import Menu from './assets/icons/menu.svg'
-import SignOut from './assets/icons/sign-out.svg'
-import Memes from './assets/icons/sun.svg'
-import AboutUs from './assets/icons/user-group.svg'
+import Icon from 'react-native-ionicons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Image } from 'react-native';
+import { BlurView } from 'expo-blur';
 
 const Tab = createBottomTabNavigator();
 
@@ -17,40 +15,23 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName='Home' screenOptions={{
-        tabBarStyle: { position: 'absolute' },
-        tabBarBackground: () => (
-          <BlurView tint="light" intensity={100} style={StyleSheet.absoluteFill} />
-        ),
-        /* tabBarActiveTintColor: '#e91e63', */
+        tabBarActiveTintColor: '#EE6983',
+        tabBarInactiveTintColor: '#850E35',
+        tabBarStyle: {position: 'absolute', paddingBottom: 10},
+        tabBarBackground: () => (<BlurView tint='light' intensity={100} style={StyleSheet.absoluteFill} />)
       }}>
-        <Tab.Screen name="Home" component={Home}
-          options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <Menu color={'#850E35'} size={30} />
-          ),
-        }} />
-        <Tab.Screen name='AboutUs' component={AboutUs} 
-          options={{
-          tabBarLabel: 'About Us',
-          tabBarIcon: ({ color, size }) => (
-            <AboutUs color={'#850E35'} size={30} />
-          ),
-        }} />
-        <Tab.Screen name='Memes' component={Memes} 
-          options={{
-          tabBarLabel: 'Memes',
-          tabBarIcon: ({ color, size }) => (
-            <Memes color={'#850E35'} size={30} />
-          ),
-        }} />
-        <Tab.Screen name='SignOut' component={SignOut} 
-          options={{
-          tabBarLabel: 'SignOut',
-          tabBarIcon: ({ color, size }) => (
-            <SignOut color={'#850E35'} size={30} />
-          ),
-        }} />
+        <Tab.Screen name='Home' component={Home} navigationOptions={{
+            tabBarIcon: ({ size, color }) => (<Icon name={'home'} color={color} size={size} />)
+          }} />
+        <Tab.Screen name='AboutUs' component={AboutUs} navigationOptions={{
+            tabBarIcon: ({ size, color }) => (<Icon name={'people-outline'} color={color} size={size} />)
+        }}/>
+        <Tab.Screen name='Memes' component={Memes} navigationOptions={{
+            tabBarIcon: ({ size, color }) => (<Icon name={'happy-outline'}   color={color} size={size} />)
+        }}/>
+        <Tab.Screen name='SignOut' component={SignOut} navigationOptions={{
+            tabBarIcon: ({ size, color }) => (<Icon name={'sign-out-outline'}   color={color} size={size} />)
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
