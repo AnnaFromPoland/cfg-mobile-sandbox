6e98b037ba1ea7c60a56554028eccf6587646806
tenko <tenko@macbook-air-anna.home>
2022-09-13 20:01:58 +0200

    Memes Added, Resize & Scroll Fixed

--- screens/Memes.js
+++ screens/Memes.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 
 export default function Memes() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>4 Best Procrastination Memes</Text>
-      <Image source={require('./assets/images/phd071307s.gif')} />
-      <Image source={require('./assets/images/phd073010s.gif')} />
-      <Image source={require('./assets/images/phd092910s.gif')} />
-      <Image source={require('./assets/images/sarah_anderson.jpg')} />
-      <StatusBar style="auto" />
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.title}>4 Best Procrastination Memes</Text>
+        <Image style={styles.image} source={require('../assets/images/phd071307s.gif')} />
+        <Image style={styles.image} source={require('../assets/images/phd073010s.gif')} />
+        <Image style={styles.image} source={require('../assets/images/phd092910s.gif')} />
+        <Image style={styles.image} source={require('../assets/images/sarah_anderson.jpg')} />
+        <StatusBar style="auto" />
+      </ScrollView>
     </View>
   );
 }
@@ -19,7 +21,11 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#C0C0C0',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+  },
+  scrollView: {
+    marginHorizontal: 10,
+    marginVertical: 50,
   },
   title: {
     fontSize: 26,
@@ -31,8 +37,10 @@ const styles = StyleSheet.create({
     lineHeight: 26,
     fontStyle: 'italic'
   },
-  text: {
-    fontSize: 14,
-    lineHeight: 16,
+  image: {
+    flex: 1,
+    width: '100%',
+    aspectratio: 1,
+    resizeMode: 'contain'
   }
 });
