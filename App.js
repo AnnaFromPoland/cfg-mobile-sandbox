26c4aa81bd848d5d1e1e9436b4e6bdea29ab38cd
tenko <tenko@macbook-air-anna.home>
2022-09-14 00:52:40 +0200

    Finally the Icons Work in Tab Nav Bar

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import Home from './screens/Home';
 import AboutUs from './screens/AboutUs';
 import Memes from './screens/Memes';
 import SignOut from './screens/SignOut';
-import Icon from 'react-native-ionicons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { StyleSheet, Image } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { BlurView } from 'expo-blur';
 
 const Tab = createBottomTabNavigator();
@@ -17,20 +17,20 @@ export default function App() {
       <Tab.Navigator initialRouteName='Home' screenOptions={{
         tabBarActiveTintColor: '#EE6983',
         tabBarInactiveTintColor: '#850E35',
-        tabBarStyle: {position: 'absolute', paddingBottom: 10},
+        tabBarStyle: {position: 'absolute'},
         tabBarBackground: () => (<BlurView tint='light' intensity={100} style={StyleSheet.absoluteFill} />)
       }}>
-        <Tab.Screen name='Home' component={Home} navigationOptions={{
-            tabBarIcon: ({ size, color }) => (<Icon name={'home'} color={color} size={size} />)
+        <Tab.Screen name='Home' component={Home} options={{
+            tabBarIcon: ({ size, color }) => (<MaterialCommunityIcons name='home-outline' color={color} size={size} />)
           }} />
-        <Tab.Screen name='AboutUs' component={AboutUs} navigationOptions={{
-            tabBarIcon: ({ size, color }) => (<Icon name={'people-outline'} color={color} size={size} />)
+        <Tab.Screen name='AboutUs' component={AboutUs} options={{
+            tabBarIcon: ({ size, color }) => (<MaterialCommunityIcons name='account-multiple' color={color} size={size} />)
         }}/>
-        <Tab.Screen name='Memes' component={Memes} navigationOptions={{
-            tabBarIcon: ({ size, color }) => (<Icon name={'happy-outline'}   color={color} size={size} />)
+        <Tab.Screen name='Memes' component={Memes} options={{
+            tabBarIcon: ({ size, color }) => (<MaterialCommunityIcons name='emoticon-happy' color={color} size={size} />)
         }}/>
-        <Tab.Screen name='SignOut' component={SignOut} navigationOptions={{
-            tabBarIcon: ({ size, color }) => (<Icon name={'sign-out-outline'}   color={color} size={size} />)
+        <Tab.Screen name='SignOut' component={SignOut} options={{
+            tabBarIcon: ({ size, color }) => (<MaterialCommunityIcons name='logout' color={color} size={size} />)
         }}/>
       </Tab.Navigator>
     </NavigationContainer>
